04268627dafcaf033d7f3f33726dbabe5b727a41
RooftoproomWhale <gh8238@naver.com>
2022-08-24 20:09:34 +0900

    최솟값 만들기 풀이

---
+++ src/programmers/level_2/최소값만들기/Solution.java
@@ -0,0 +1,19 @@
+package programmers.level_2.최소값만들기;
+
+import java.util.Arrays;
+
+public class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for(int i = 0; i < A.length; i++)
+        {
+            answer += A[i] * B[A.length-i-1];
+        }
+
+        return answer;
+    }
+}
